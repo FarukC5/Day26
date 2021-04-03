e0ea6ab428c9d0a42a122b9b0ab7f8968fa2da4e
FarukC5 <faruk_cajic@yahoo.com>
2021-04-03 04:53:39 +0200

    Axios - POST async/await

---
+++ ex06/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import PostReactAsyncAwait from "./PostRequestAsyncAwait";
+
+function App() {
+  return (
+    <div className="App">
+      <PostReactAsyncAwait />
+    </div>
+  );
+}
+
+export default App;
